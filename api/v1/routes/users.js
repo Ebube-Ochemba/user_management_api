0f5baacbd5ee3d23d990053a0d35e8f650856eda
Ebube-Ochemba <ebube17@gmail.com>
2024-08-21 00:15:14 +0100

    documentation

--- api/v1/routes/users.js
+++ api/v1/routes/users.js
@@ -12,10 +12,10 @@ router.post("/", userController.createUser)
 // Getting one user by name or ID
 router.get("/:id?", userMiddleware.getUser, userController.getUserById)
 
-// Updating one user  or ID
+// Updating one user or ID
 router.patch("/:id?", userMiddleware.getUser, userController.updateUser)
 
-// Deleting one user  or ID
+// Deleting one user or ID
 router.delete("/:id?", userMiddleware.getUser, userController.deleteUser)
 
 module.exports = router
