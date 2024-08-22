e2cc4ec7888b223bfb03a5b60a3b58e79e10399a
Ebube-Ochemba <ebube17@gmail.com>
2024-08-22 19:06:03 +0100

    updated for prod

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ require('dotenv').config()
 
 const express = require('express')
 const db = require('./config/db')
+const userRouter = require('./api/v1/routes/users')
+
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(express.json()) // Middleware to accept JSON in requests
+// Middleware to accept JSON in requests
+app.use(express.json())
 
-const userRouter = require('./api/v1/routes/users')
+// Routes
 app.use('/', userRouter)
 
 
+// Start derver
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
 })
