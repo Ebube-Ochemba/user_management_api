6ad7366edb663f0874bf9e2bc768a6ede14887c7
Ebube-Ochemba <ebube17@gmail.com>
2024-08-25 23:23:49 +0100

    refactored

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const userRouter = require('./api/v1/routes/users')
 const app = express()
 const port = process.env.PORT || 3001
 
+// Connect to database
+connectDB();
+
 // Middleware to accept JSON in requests
 app.use(express.json())
 
@@ -21,7 +24,7 @@ if (process.env.NODE_ENV !== 'prod') {
     // Start Server
     app.listen(port, () => {
         console.log(`Server running at http://localhost:${port}`);
-        connectDB(); // Connect to database
+
     })
 }
 
