792cf8a188c6c7995e0b4ce82a0e8564e4fd668a
Ebube-Ochemba <ebube17@gmail.com>
2024-08-25 12:37:39 +0100

    refactoredto ensure DB connection for serverless function

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ require('dotenv').config()
 
 // Imports
 const express = require('express')
-const db = require('./config/db')
+const connectDB = require('./config/db')
 const userRouter = require('./api/v1/routes/users')
 
 // App setup
 const app = express()
 const port = process.env.PORT || 3001
+
+// Connect to database
+connectDB();
 
 // Middleware to accept JSON in requests
 app.use(express.json())
@@ -17,10 +20,11 @@ app.use(express.json())
 app.use('/', userRouter)
 
 
-// Start Server
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-})
-
+if (process.env.NODE_ENV == 'prod') {
+    // Start Server
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`)
+    })
+}
 
 module.exports = app
